26ad0383356ebc1e87710743e5e3a4a8dea2abb5
Ryan Walsh <rmwalshy@gmail.com>
2016-08-06 16:31:04 -0400

    Initial push

---
+++ tweet-reader.py
@@ -0,0 +1,62 @@
+'''
+tweet-reader.py
+
+Version: .1
+
+'''
+import tweepy
+
+auth = tweepy.OAuthHandler('JJeRQVU3HNIjUVxjDlQBMp58i', 'pnREV3Fhejx9j6rf4RTc0TC1nBGGLdKlwZADbk3D33gwdEbj61')
+auth.set_access_token('179118313-tKTnnf2MTxBKK9oYgfrKpg3XDVTqM0A4DqWZjwJV', 'xfadRgFXQCGgETOsQLHiHlbMCIZ5WUhpahMngu0TqwzVl')
+
+api = tweepy.API(auth)
+
+twitterID = input('Enter the person you are looking for\n')
+
+topWords = input('Enter the words you are searching for (with spaces inbetween each)\n').lower()
+
+
+numTweets = 500
+#public_tweets = api.user_timeline(id = 'realDonaldTrump', count = numTweets)
+
+
+def process_tweet(status):
+
+	return status.text.replace('.', ' ').replace(',', ' ').replace('!', ' ').replace('"', '').replace('-', ' ').lower().split()
+
+
+Hash = {}
+
+# hash all tweets
+for status in tweepy.Cursor(api.user_timeline, id='realDonaldTrump').items(numTweets):
+
+	for word in process_tweet(status):
+
+		# add all words to hash count
+		if(word not in Hash):
+			Hash[word] = 1
+		else:
+			Hash[word]+= 1
+
+
+print('\nSearched Words of last 200 tweets:')
+
+for topWord in topWords.split():
+
+	value = Hash[topWord]
+
+	print('    ' + topWord + ': ' + str(value) + ', %' + str(value * 100 / numTweets) + ' of tweets')
+
+
+
+print('\nOther Top Words:')
+
+count = 0
+
+for w in sorted(Hash, key=Hash.get, reverse=True):
+
+	if count > 10: break
+
+	if w not in ('the', 'a', 'it', 'to', 'so', 'be', 'it', 'i', 'is', 'of', 'in', 'and', 'https://t', 'on', 'for', 'was', 'with'):
+		print('    ' + w + ': ' + (str)(Hash[w]))
+		count+= 1
